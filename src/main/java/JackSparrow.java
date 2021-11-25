fb30569211e1ccbe502869e08e84320e9ff817fd
Arqam Hussain <arqamh@collab-lab.co>
2021-11-25 12:09:19 +0530

    Added test classes and another main class

---
+++ src/main/java/JackSparrow.java
@@ -0,0 +1,5 @@
+public class JackSparrow {
+    public static void main(String[] args) {
+        System.out.println("Captain and Calypso");
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,8 @@
+public class HelloWorldTest {
+    public void testHello() {
+        System.out.println("Test - Hello...");
+    }
+    public void testWord() {
+        System.out.println("Test - World !!!");
+    }
+}
